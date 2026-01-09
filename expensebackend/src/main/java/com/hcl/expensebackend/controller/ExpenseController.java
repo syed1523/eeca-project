9d7169e813d1fca4632c3ca6c86e5f9c09a802ed
syed1523 <syedabuthahirmr@gmail.com>
2026-01-09 15:06:24 +0530

    Updated code

--- expensebackend/src/main/java/com/hcl/expensebackend/controller/ExpenseController.java
+++ expensebackend/src/main/java/com/hcl/expensebackend/controller/ExpenseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/expenses")
 public class ExpenseController {
