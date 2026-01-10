388930d074cd87047ad6fe09fbcfa51f0a3007b0
syed1523 <syedabuthahirmr@gmail.com>
2026-01-10 10:49:08 +0530

    Updated code

--- expensebackend/src/main/java/com/hcl/expensebackend/controller/ExpenseController.java
+++ expensebackend/src/main/java/com/hcl/expensebackend/controller/ExpenseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = {
-        "http://localhost:5173",
+
         "https://eeca-project-mg1668jlz-syeds-projects-a197ffd1.vercel.app"
 })
 @RestController
